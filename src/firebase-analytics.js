e22d510df062a111de8651ac6fea7e4d2dfe4172
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-10-06 22:46:36 +0700

    .

---
+++ src/firebase-analytics.js
@@ -0,0 +1,18 @@
+// I create this file because I don't want to modify the firebase.js file
+
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCaDBpg8plpGlFlSl1ke7gH_lJKZ4VRjfs",
+    authDomain: "d2-shop.firebaseapp.com",
+    databaseURL: "https://d2-shop.firebaseio.com",
+    projectId: "d2-shop",
+    storageBucket: "d2-shop.appspot.com",
+    messagingSenderId: "488705744297",
+    appId: "1:488705744297:web:1f8d98ef2038ca788af8b8",
+    measurementId: "G-37DR1GJ7DF"
+  };
+
+var fireDb = firebase.initializeApp(firebaseConfig);
+
+export default fireDb.analytics();
