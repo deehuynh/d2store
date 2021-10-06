953a8c2210a21f1336f7f30899459f31bfb237a0
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-10-06 23:30:33 +0700

    .

--- src/Components/CP/Login.js
+++ src/Components/CP/Login.js
@@ -2,7 +2,17 @@ import '../../Style/admin-login.scss'
 
 function Login () {
   return (
-    <div className="admin-login"></div>
+    <div className="admin-login">
+      <form className="admin-login__form">
+        <Input />
+      </form>
+    </div>
+  )
+}
+
+function Input (props) {
+  return (
+    <input type="text" defaultValue='' placeholder={props.placeholder} />
   )
 }
 
